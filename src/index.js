be2f7a55fa7e016da518f0c92ee64e9e0825d7e7
joyu <775154773@qq.com>
2016-11-21 20:26:31 +0800

    upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Transbox from './Trans/Trans.js';
+import './index.css';
+
+ReactDOM.render(
+  <Transbox />,
+  document.getElementById('root')
+);
